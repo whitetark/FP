6ed14d4af15ca9ba551b7684e91a70c069b85708
Sviatoslav Chapcha <whitetark@gmail.com>
2022-10-14 21:15:17 +0300

    lab01 is ready

--- lab01/pursv/src/Main.purs
+++ lab01/pursv/src/Main.purs
@@ -4,7 +4,40 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Console (log)
+import Data.List
+
+singleton :: forall a. a -> List a
+singleton a = a : Nil
+
+null :: forall a. List a -> Boolean
+null Nil = true
+null _ = false
+
+snoc :: forall a. List a -> a -> List a
+snoc array insertElem = reverse (insertElem : reverse array)
+
+length :: forall a. List a -> Int
+length Nil = 0
+length (Cons _ array) = 1 + length array
+
+firstTask :: Int
+firstTask = 29
+
+secondTask :: List Int
+secondTask = Nil
+
+thirdTask :: List String
+thirdTask = "Oh" : "mamma" : "mia" : Nil
+
+insertLastElement :: String
+insertLastElement = "fake info"
+
+fourthTask :: List String
+fourthTask = "The" : "best of" : "times" : Nil
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  log ("First Task:" <> show (singleton firstTask))
+  log ("Second Task:" <> show (null secondTask))
+  log ("Third Task:" <> show (snoc thirdTask insertLastElement))
+  log ("Forth Task:" <> show (length fourthTask))
